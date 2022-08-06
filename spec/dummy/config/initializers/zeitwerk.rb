63ed405f50e5de6a03a266d575c1fc2021826686
Joshua Buermann <buermann@gmail.com>
2022-08-05 17:42:34 -0700

    update test suite rails to latest 5.x

--- spec/dummy/config/initializers/zeitwerk.rb
+++ spec/dummy/config/initializers/zeitwerk.rb
@@ -1,6 +1,8 @@
-Rails.autoloaders.main.ignore(
-"app/admin",
-"app/assets",
-"app/javascripts",
-"app/views"
-)
+if Rails::VERSION::MAJOR > 5
+  Rails.autoloaders.main.ignore(
+    "app/admin",
+    "app/assets",
+    "app/javascripts",
+    "app/views"
+  )
+end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #devise_for :admin_users, ActiveAdmin::Devise.config
+  #devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
   ActiveAdmin.routes(self)
 
